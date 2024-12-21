cd7ba29292c86322fb87342e2762cf1582fa7a6f
Subash Bhattarai <subash@macbookair.home>
2024-12-21 13:18:52 +0000

    single page

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,4 +16,29 @@ jQuery(document).ready(function ($) {
         $('html, body').animate({ scrollTop: 0 }, 600);
         return false;
     });
+
+
+    // code copy button
+    $('.code-block').prepend('<button class="copy-btn"><i class="fa fa-copy"></i> Copy</button>');
+    $(".copy-btn").on("click", function () {
+        // Find the <code> element in the same container
+        const codeText = $(this).siblings("pre").find("code").text().trim();
+
+        // Copy text to clipboard
+        navigator.clipboard.writeText(codeText).then(() => {
+            // Show success feedback
+            const button = $(this);
+            button.text("Copied!");
+            setTimeout(() => {
+                button.text("Copy");
+            }, 1500);
+        }).catch((err) => {
+            console.error("Failed to copy text: ", err);
+            const button = $(this);
+            button.text("Error");
+            setTimeout(() => {
+                button.text("Copy");
+            }, 1500);
+        });
+    });
 });
